20ac86017432c4c0581ef9634d5170a0f54027c4
Dennis <dennis.kimani@student.moringaschool.com>
2021-06-16 14:39:18 +0300

    updated apps

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,14 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_movies,get_movie,search_movie
+from .forms import ReviewForm
+from ..models import Review
+
+# Views
 @main.route('/')
 def index():
-    return '<h1> Hello World </h1>'
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    return render_template('index.html')
