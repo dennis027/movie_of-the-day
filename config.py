cf7e101bbaec412e68a7ce841d182df31c3cbb34
jLuseno161 <joyluseno2@gmail.com>
2021-06-17 21:39:32 +0300

    final changes

--- config.py
+++ config.py
@@ -6,30 +6,23 @@ class Config:
     MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    UPLOADED_PHOTOS_DEST = 'app/static/photos'
-
     GENRES_URL ='https://api.themoviedb.org/3/genre/movie/list?api_key={}'
     GENRE_MOVIES_URL = 'https://api.themoviedb.org/3/discover/movie?api_key={}&with_genres={}'
 
-    # simple mde  configurations
-    SIMPLEMDE_JS_IIFE = True
-    SIMPLEMDE_USE_CDN = True
+
     @staticmethod
     def init_app(app):
         pass
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     pass
 
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jluseno@localhost/watchlist_test'
-
+    pass
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jluseno@localhost/watchlist'
 
     DEBUG = True
 
