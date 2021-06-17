fb3ad8705807f216987fdde0662a67600fb21536
Dennis <dennis.kimani@student.moringaschool.com>
2021-06-17 10:49:05 +0300

    adding login credentials

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from . import main
+from flask_login import login_required
 
 
 # from ..models import 
@@ -12,3 +13,9 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@login_required
+def new_review(id):
+    ''''''
